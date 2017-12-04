0761bc9ccc8390102859d2e836ceb6767308a39f
alizade91 <32942946+alizade91@users.noreply.github.com>
2017-12-04 23:09:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_debugging
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string key = Generate();
+			Console.WriteLine(key);
+		}
+
+		static string Generate()
+		{
+			var networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+			byte[] byteArray = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+			if (networkInterface == null)
+			{
+				throw new Exception("Something happened");
+			}
+			int[] addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes().Select((x, y) => x ^ byteArray[y])
+					  .Select(a =>
+					  {
+						  if (a > 999)
+						  {
+							  return a;
+						  }
+						  return a * 10;
+					  }).ToArray();
+
+			return string.Join("-", addressBytes);
+		}
+	}
+}
